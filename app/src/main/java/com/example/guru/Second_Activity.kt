0c24e3354c0ddd3ad4f6fcbae710c77ffdedb231
eighths <ssnnii1010@gmail.com>
2022-07-26 12:42:00 +0900

    수정

--- app/src/main/java/com/example/guru/Second_Activity.kt
+++ app/src/main/java/com/example/guru/Second_Activity.kt
@@ -1,11 +1,22 @@
 package com.example.guru
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 
 class Second_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 }
