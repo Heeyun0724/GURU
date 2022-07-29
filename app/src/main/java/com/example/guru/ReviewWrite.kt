5b28a0215c8489ff99a484573ab67b4e537bc3cd
hyunseo234 <69571570+hyunseo234@users.noreply.github.com>
2022-07-29 14:47:13 +0900

    리뷰 작성

--- app/src/main/java/com/example/guru/ReviewWrite.kt
+++ app/src/main/java/com/example/guru/ReviewWrite.kt
@@ -1,40 +1,67 @@
 package com.example.guru
 
+import android.content.Context
 import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import android.widget.*
 
 class ReviewWrite : AppCompatActivity() {
-     lateinit var btn1: RadioButton
-     lateinit var btn2: RadioButton
-     lateinit var btn3: RadioButton
-     lateinit var btn4: RadioButton
-     lateinit var btn5: RadioButton
-     lateinit var radioGroup: RadioGroup
-     lateinit var complete: Button
 
+     lateinit var bookName: EditText
+     lateinit var star : EditText
+     lateinit var review: EditText
 
+     lateinit var completeButton: Button
+     lateinit var sqlDB: SQLiteDatabase
+     lateinit var myHelper: myDBHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review_write)
 
-         btn1 = findViewById(R.id.star_radio1)
-         btn2 = findViewById(R.id.star_radio2)
-         btn3  = findViewById(R.id.star_radio3)
-         btn4  = findViewById(R.id.star_radio4)
-         btn5  = findViewById(R.id.star_radio5)
-         radioGroup = findViewById(R.id.radioGroup)
-         complete = findViewById(R.id.complete)
+        bookName = findViewById(R.id.bookname)
+        star = findViewById(R.id.star)
+        review = findViewById(R.id.review)
+
+        completeButton=findViewById(R.id.complete)
 
 
-        complete.setOnClickListener{
+        myHelper = myDBHelper(this)
+        completeButton.setOnClickListener {
+            var bname = bookName.text.toString()
+            var rstar = star.text.toString()
+            var rcontent = review.text.toString()
+
+            sqlDB = myHelper.writableDatabase
+            sqlDB.execSQL("INSERT INTO reviewTbl VALUES('" + bname + "', '" + rstar + "', '" + rcontent + "');" )
+            sqlDB.close()
+            Toast.makeText(applicationContext, "작성됨", Toast.LENGTH_SHORT).show()
+        }
 
 
-                Toast.makeText(this,"작성완료", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, Second_Activity::class.java)
-                startActivity(intent)
-            }
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    inner class myDBHelper(context: Context) : SQLiteOpenHelper(context, "reviewTbl", null, 1) {
+        override fun onCreate(db: SQLiteDatabase?) {
+            db!!.execSQL("CREATE TABLE reviewTbl ( bName CHAR(100) , rStar CHAR(20) ,rContent CHAR(1000));")
+        }
+        override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+            db!!.execSQL("DROP TABLE IF EXISTS reviewTbl")
+            onCreate(db)
         }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.getItemId() === android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
