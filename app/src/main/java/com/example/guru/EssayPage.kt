2e569a2a9ea678a03cb018a8d04033d7743f659f
hyunseo234 <69571570+hyunseo234@users.noreply.github.com>
2022-07-30 14:10:33 +0900

    Update EssayPage.kt

--- app/src/main/java/com/example/guru/EssayPage.kt
+++ app/src/main/java/com/example/guru/EssayPage.kt
@@ -8,9 +8,11 @@ class EssayPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_essay_page)
+        //액션바에 뒤로가기 추가
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 
+    //뒤로가기 코드
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.getItemId() === android.R.id.home) {
             finish()
