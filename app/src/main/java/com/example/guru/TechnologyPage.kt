ff28e17509f49b272d74bda1d7c993351e70b3db
hyunseo234 <69571570+hyunseo234@users.noreply.github.com>
2022-07-30 14:12:25 +0900

    주석 추가

--- app/src/main/java/com/example/guru/TechnologyPage.kt
+++ app/src/main/java/com/example/guru/TechnologyPage.kt
@@ -17,12 +17,12 @@ class TechnologyPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_technology_page)
-
-
+        //액션바 뒤로가기 추가
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
     }
 
+    //뒤로가기 코드
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.getItemId() === android.R.id.home) {
             finish()
