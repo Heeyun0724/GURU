291696be6e7cf5f5725d2972bc07a2862a2ed317
hyunseo234 <69571570+hyunseo234@users.noreply.github.com>
2022-07-30 15:56:21 +0900

    카테고리 페이지

---
+++ app/src/main/java/com/example/guru/EssayPageActivity.kt
@@ -0,0 +1,23 @@
+package com.example.guru
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+
+class EssayPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_essay_page)
+        //액션바 뒤로가기 추가
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    //뒤로가기 코드
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.getItemId() === android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
