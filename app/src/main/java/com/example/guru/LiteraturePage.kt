3b9ce6e872c3e3ccd15c41471592c1a94d35dfc6
hyunseo234 <69571570+hyunseo234@users.noreply.github.com>
2022-07-30 14:11:28 +0900

    주석 추가

--- app/src/main/java/com/example/guru/LiteraturePage.kt
+++ app/src/main/java/com/example/guru/LiteraturePage.kt
@@ -8,9 +8,11 @@ class LiteraturePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_literature_page)
+        //액션바 뒤로가기 추가
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 
+    //뒤로가기 코드
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.getItemId() === android.R.id.home) {
             finish()
